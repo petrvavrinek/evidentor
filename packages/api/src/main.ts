797c71ea101e9acfc4422aed8d16b7b09d6e647a
Petr Vavřínek <vavrinekpetr155@gmail.com>
2025-05-27 18:38:33 +0200

    feat: add openapi and swagger route

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,14 +1,35 @@
 import { serve } from "@hono/node-server";
+import { swaggerUI } from "@hono/swagger-ui";
 import { Hono } from "hono";
+import { openAPISpecs } from "hono-openapi";
 import logger from "./logger";
 
 import { userApp } from "./routers/user.router";
+import env from "./env";
 
 const app = new Hono().route("/user", userApp);
 
 const server = serve(app, ({ address, port }) => {
   logger.info(`Server listening on ${address}:${port}`);
 });
+
+if (env.IsDevelopment) {
+  app.use(
+    "/openapi",
+    openAPISpecs(app, {
+      documentation: {
+        info: {
+          title: "Evidentor API",
+          version: "0.0.1",
+        },
+      },
+    })
+  );
+  logger.info("OpenAPI registered");
+
+  app.use("/openapi/ui", swaggerUI({ url: "/openapi" }));
+  logger.info("Swagger endpoint registered");
+}
 
 // According to docs: graceful shutdown
 // Source: https://hono.dev/docs/getting-started/nodejs
