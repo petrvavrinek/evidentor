40c7e7a66409790c7f69c659d9616990625bc740
Petr Vavřínek <vavrinekpetr155@gmail.com>
2025-06-15 13:57:56 +0200

    feat: openapi fetch generator

---
+++ packages/web/openapi-ts.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "@hey-api/openapi-ts";
+
+export default defineConfig({
+  input: "http://localhost:3000/swagger/json",
+  output: {
+    format: "prettier",
+    lint: "eslint",
+    path: "./src/lib/api",
+  },
+  plugins: [
+    "@hey-api/client-fetch",
+    "@hey-api/schemas",
+    "zod",
+    {
+      name: "@hey-api/sdk",
+      validator: true,
+    },
+    {
+      enums: "javascript",
+      name: "@hey-api/typescript",
+    },
+    "@tanstack/react-query",
+  ],
+});
